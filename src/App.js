62e52b798637b4d1601d7ca790f80077dacb8460
Maxime <82838299+hallomaxime@users.noreply.github.com>
2024-06-03 15:51:50 +0200

    added footer

--- src/App.js
+++ src/App.js
@@ -4,9 +4,31 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Weather App</h1>
-      <SearchEngine />
+    <div>
+      <div className="App">
+        <h1>Weather App</h1>
+        <SearchEngine />
+      </div>
+      <div>
+        <footer>
+          This project was coded by Maxime Spithorst 🙋🏼‍♀️, is open sourced on{" "}
+          <a
+            href="https://www.github.com/hallomaxime"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://graceful-douhua-be3de5.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
